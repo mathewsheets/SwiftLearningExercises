06a52c6ed55f3dd166ee4505ffe4e818aafc504a
Mathew Sheets <mathew.sheets@gmail.com>
2016-02-17 04:13:52 -0500

    more like a spread sheet

--- Exercise_4.playground/Contents.swift
+++ Exercise_4.playground/Contents.swift
@@ -18,6 +18,28 @@
     * isStrictSupersetOf
     * isDisjointWith
 */
+// Data
+var rows = [
+    ["column_1" : "Obi-Wan",   "column_2" : "Kenobi"],
+    ["column_1" : "Darth",     "column_2" : "Vader"],
+    ["column_1" : "Anakin",    "column_2" : "Skywalker"],
+    ["column_1" : "Darth",     "column_2" : "Sidious"],
+    ["column_1" : "Padme",     "column_2" : "Amidala"],
+    ["column_1" : "Mace",      "column_2" : "Windu"],
+    ["column_1" : "Count",     "column_2" : "Dooku"],
+    ["column_1" : "Luke",      "column_2" : "Skywalker"],
+    ["column_1" : "Han",       "column_2" : "Solo"],
+    ["column_1" : "Leia",      "column_2" : "Organa"],
+    ["column_1" : "Chew",      "column_2" : "Bacca"],
+    ["column_1" : "Boba",      "column_2" : "Fett"],
+    ["column_1" : "Lando",     "column_2" : "Calrissian"],
+    ["column_1" : "Kylo",      "column_2" : "Ren"],
+    ["column_1" : "Poe",       "column_2" : "Dameron"],
+    ["column_1" : "Finn",      "column_2" : "FN-2187"],
+    ["column_1" : "Rey",       "column_2" : "Rey"]
+]
+
+// Data Types
 typealias StudentKey    = String
 typealias StudentValue  = String
 typealias Student       = [StudentKey:StudentValue]
@@ -29,83 +51,51 @@ typealias Class         = Set<StudentName>
 typealias ClassName     = String
 typealias Classes       = [ClassName:Class]
 
-var matt        = Student(); matt       ["name"] = "Matt";      matt    ["last"] = ""
-var annie       = Student(); annie      ["name"] = "Annie";
-var sam         = Student(); sam        ["name"] = "Sam";
-var jack        = Student(); jack       ["name"] = "Jack";
-var hudson      = Student(); hudson     ["name"] = "Hudson";
-var oliver      = Student(); oliver     ["name"] = "Oliver";
-var carson      = Student(); carson     ["name"] = "Carson";
-var rick        = Student(); rick       ["name"] = "Rick";
-var joanne      = Student(); joanne     ["name"] = "Joanne";
-var jennifer    = Student(); jennifer   ["name"] = "Jennifer";
-var brian       = Student(); brian      ["name"] = "Brian";
-var danielle    = Student(); danielle   ["name"] = "Danielle";
-var mike        = Student(); mike       ["name"] = "Mike";
-var carl        = Student(); carl       ["name"] = "Carl";
-var becky       = Student(); becky      ["name"] = "Becky";
-var amber       = Student(); amber      ["name"] = "Amber";
-var todd        = Student(); todd       ["name"] = "Todd";
-
-var students = Students()
-students.append(matt)
-students.append(annie)
-students.append(sam)
-students.append(jack)
-students.append(hudson)
-students.append(oliver)
-students.append(carson)
-students.append(rick)
-students.append(joanne)
-students.append(jennifer)
-students.append(brian)
-students.append(danielle)
-students.append(mike)
-students.append(carl)
-students.append(becky)
-students.append(amber)
-students.append(todd)
-
+// Processing
 var allStudents = Set<String>()
-for student in students {
-    allStudents.insert(student["name"]!)
+for (index, var student) in rows.enumerate() {
+    let name = "\(student["column_1"]!) \(student["column_2"]!)"
+    student["name"] = name
+    
+    rows[index] = student
+    allStudents.insert(name)
 }
 
 var math = Class()
-math.insert(students[8]["name"]!)
-math.insert(students[4]["name"]!)
-math.insert(students[13]["name"]!)
-math.insert(students[11]["name"]!)
-math.insert(students[7]["name"]!)
-math.insert(students[0]["name"]!)
-math.insert(students[6]["name"]!)
+math.insert(rows[8]["name"]!)
+math.insert(rows[4]["name"]!)
+math.insert(rows[13]["name"]!)
+math.insert(rows[11]["name"]!)
+math.insert(rows[7]["name"]!)
+math.insert(rows[0]["name"]!)
+math.insert(rows[6]["name"]!)
 
 var science = Class()
-science.insert(students[7]["name"]!)
-science.insert(students[12]["name"]!)
-science.insert(students[10]["name"]!)
-science.insert(students[4]["name"]!)
-science.insert(students[14]["name"]!)
-science.insert(students[6]["name"]!)
-science.insert(students[1]["name"]!)
+science.insert(rows[7]["name"]!)
+science.insert(rows[12]["name"]!)
+science.insert(rows[10]["name"]!)
+science.insert(rows[4]["name"]!)
+science.insert(rows[14]["name"]!)
+science.insert(rows[6]["name"]!)
+science.insert(rows[1]["name"]!)
 
 var english = Class()
-english.insert(students[3]["name"]!)
-english.insert(students[5]["name"]!)
-english.insert(students[13]["name"]!)
-english.insert(students[16]["name"]!)
-english.insert(students[6]["name"]!)
-english.insert(students[2]["name"]!)
-english.insert(students[9]["name"]!)
+english.insert(rows[3]["name"]!)
+english.insert(rows[5]["name"]!)
+english.insert(rows[13]["name"]!)
+english.insert(rows[16]["name"]!)
+english.insert(rows[6]["name"]!)
+english.insert(rows[2]["name"]!)
+english.insert(rows[9]["name"]!)
 
 var history = Class()
-history.insert(students[7]["name"]!)
-history.insert(students[0]["name"]!)
-history.insert(students[15]["name"]!)
-history.insert(students[2]["name"]!)
-history.insert(students[4]["name"]!)
-history.insert(students[6]["name"]!)
-history.insert(students[5]["name"]!)
+history.insert(rows[7]["name"]!)
+history.insert(rows[0]["name"]!)
+history.insert(rows[15]["name"]!)
+history.insert(rows[2]["name"]!)
+history.insert(rows[4]["name"]!)
+history.insert(rows[6]["name"]!)
+history.insert(rows[5]["name"]!)
 
 var classes = Classes()
 classes["Math"] = math
@@ -113,8 +103,8 @@ classes["Science"] = science
 classes["Engligh"] = english
 classes["History"] = history
 
-print("All \(students.count) students")
-for student in students {
+print("All \(rows.count) students")
+for student in rows {
     print("\t\(student["name"]!)")
 }
 
@@ -165,15 +155,15 @@ if allStudents.isSupersetOf(history) {
 }
 
 var mathBoys = Class()
-mathBoys.insert(students[13]["name"]!)
-mathBoys.insert(students[0]["name"]!)
-mathBoys.insert(students[7]["name"]!)
-mathBoys.insert(students[6]["name"]!)
-mathBoys.insert(students[4]["name"]!)
+mathBoys.insert(rows[13]["name"]!)
+mathBoys.insert(rows[0]["name"]!)
+mathBoys.insert(rows[7]["name"]!)
+mathBoys.insert(rows[6]["name"]!)
+mathBoys.insert(rows[4]["name"]!)
 
 var mathGirls = Class()
-mathGirls.insert(students[11]["name"]!)
-mathGirls.insert(students[8]["name"]!)
+mathGirls.insert(rows[11]["name"]!)
+mathGirls.insert(rows[8]["name"]!)
 
 if mathGirls.isStrictSubsetOf(math) {
     print("Math girls is a strict subset of math")
