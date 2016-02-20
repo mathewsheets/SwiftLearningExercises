a15aa7c7f46fa071288e534cddb8b531cab37432
Mathew Sheets <mathew.sheets@gmail.com>
2016-02-20 02:08:23 -0500

    Shell for exercise 5

---
+++ Exercise_5.playground/Pages/Exercises.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Next](@next)
+/*:
+**Exercise:** Create a playground with pages with each playground page consisting of the previous four exercises. Refactor each exercise leveraging collection types and functions.
+*/
+/*:
+- [Exercise 1](Exercise%201)
+- [Exercise 2](Exercise%202)
+- [Exercise 3](Exercise%203)
+- [Exercise 4](Exercise%204)
+*/
+//: [Next](@next)
---
+++ Exercise_5.playground/Sources/Common.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+public func hello(name: String) -> String {
+    
+    return "Hello \(name)!"
+}
