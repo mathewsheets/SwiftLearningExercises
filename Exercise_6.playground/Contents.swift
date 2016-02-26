c4639b9fcf6a6fe867fc7ea1d03df1aef226c9e2
Mathew Sheets <mathew.sheets@gmail.com>
2016-02-26 08:46:27 -0500

    misspelling

--- Exercise_6.playground/Contents.swift
+++ Exercise_6.playground/Contents.swift
@@ -46,5 +46,5 @@ print("filter: \n\t\(filter(students) { $0["class"]! == "Engligh" }!)")
 print("reject: \n\t\(reject(students) { Int($0["age"]!) >= 21 }!)")
 print("pluck: \n\t\(pluck(students) { $0["age"]! }.sort())")
 print("pluck: \n\t\(pluck(students) { $0["last"]! })")
-print("Last names of math and sciense students where age > 25 and age < 80\n\t\(pluck(filter(filter(students) { $0["class"] == "Math" }! + filter(students) { $0["class"] == "Science" }!) { Int($0["age"]!)! > 25 && Int($0["age"]!)! < 80 }!) { $0["last"]! })")
+print("Last names of math and science students where age > 25 and age < 80\n\t\(pluck(filter(filter(students) { $0["class"] == "Math" }! + filter(students) { $0["class"] == "Science" }!) { Int($0["age"]!)! > 25 && Int($0["age"]!)! < 80 }!) { $0["last"]! })")
 
