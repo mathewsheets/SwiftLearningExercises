b5d868f7f433b35aae8047694f5d459eca7f5fd7
Mathew Sheets <mathew.sheets@gmail.com>
2016-05-14 07:34:19 -0400

    Start of exercise 15

---
+++ Exercise_15.playground/Contents.swift
@@ -0,0 +1,13 @@
+/*:
+ * callout(Exercise): Create a Person API by creating a protocol and two classes conforming to the protocol with methods to create, retrieve, update and delete persons. One of the classes should *mock* the HTTP Client for the `GET` routes reading files for the JSON payload. The other class should leverage the above examples.
+ 
+ **Constraints:**
+ - Protocol with methods to create, retrieve, update and delete person instances
+ - Class conforming to the protocol reading files for the JSON payloads
+ - Class conforming to the protocol using NSURLRequest, NSURLResponse and NSURLSession
+ - Each method body must use the appropriate example when implementing the HTTP Client class
+ - Each method body must use what you learned in [JSON & Instances](JSON%20and%20Instances) to serialize/deserialize a person instances
+ */
+import Foundation
+import XCPlayground
+
